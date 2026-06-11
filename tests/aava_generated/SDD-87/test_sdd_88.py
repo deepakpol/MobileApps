bfbac04bb07d06cbefbd3176b34d5636c2e359f5
deepakpol <deepakpol@gmail.com>
2026-06-11 15:21:23 -0400

    AAVA: generated test for SDD-88 (SDD-87)

--- tests/aava_generated/SDD-87/test_sdd_88.py
+++ tests/aava_generated/SDD-87/test_sdd_88.py
@@ -1,11 +1,12 @@
-from django.test import SimpleTestCase
+import unittest
 from django.utils.text import slugify
 
 
-class SlugifySpacesToHyphensTest(SimpleTestCase):
-    def test_spaces_become_single_hyphens_and_text_is_lowercased(self):
+class TestSlugifySpacesToHyphens(unittest.TestCase):
+    def test_spaces_become_single_hyphens_and_text_lowercased(self):
         """
-        Given 'Hello World Foo', when slugify is applied,
+        Test that spaces become single hyphens and text is lowercased.
+        Given 'Hello World Foo', when slugify is applied, 
         then the result is 'hello-world-foo'.
         """
         result = slugify('Hello World Foo')
