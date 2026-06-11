17b1b482472c94d41d78c5cc63713762ef3fe42c
deepakpol <deepakpol@gmail.com>
2026-06-11 15:21:24 -0400

    AAVA: generated test for SDD-89 (SDD-87)

--- tests/aava_generated/SDD-87/test_sdd_89.py
+++ tests/aava_generated/SDD-87/test_sdd_89.py
@@ -1,13 +1,16 @@
-from django.test import SimpleTestCase
+import unittest
 from django.utils.text import slugify
 
 
-class SlugifyPunctuationTest(SimpleTestCase):
-    def test_slugify_strips_disallowed_punctuation(self):
+class TestSlugifyPunctuationStripping(unittest.TestCase):
+    def test_disallowed_punctuation_is_stripped(self):
         """
-        Test that disallowed punctuation is stripped.
-        Given 'C++ & Python: A Guide!', when slugify is applied,
-        then punctuation is removed and the result is 'c-python-a-guide'.
+        Test that punctuation is removed when slugify is applied.
+        Given 'C++ & Python: A Guide!', the result should be 'c-python-a-guide'.
         """
-        result = slugify('C++ & Python: A Guide!')
-        self.assertEqual(result, 'c-python-a-guide')
+        input_string = 'C++ & Python: A Guide!'
+        expected_output = 'c-python-a-guide'
+        
+        result = slugify(input_string)
+        
+        self.assertEqual(result, expected_output)
